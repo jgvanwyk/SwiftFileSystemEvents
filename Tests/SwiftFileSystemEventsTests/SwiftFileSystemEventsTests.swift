3f27626cecd50f9fcf90d41df955d4302a36c868
jgvanwyk <123784102+jgvanwyk@users.noreply.github.com>
2026-04-20 00:20:52 -0400

    Remove invalid test

--- Tests/SwiftFileSystemEventsTests/SwiftFileSystemEventsTests.swift
+++ Tests/SwiftFileSystemEventsTests/SwiftFileSystemEventsTests.swift
@@ -28,10 +28,4 @@ final class SwiftFileSystemEventsTests: XCTestCase {
         XCTAssert(flags1.description == "[itemCreated, itemCloned]")
     }
     
-    func testFileSystemEventStreamDirectoriesBeingWatched() {
-        let url = FileManager.default.temporaryDirectory
-        let stream = FileSystemEventStream(directoriesToWatch: [url], handler: { print($0) })
-        XCTAssert(stream.directoriesBeingWatched == [url])
-    }
-    
 }
